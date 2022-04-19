ee22481390964eb319847ea85a2df1edf7c23e92
Abad <abdila.alid@gmail.com>
2022-04-19 09:05:04 +0700

    Ubah sedikit

--- MetaKantin.js
+++ MetaKantin.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');;
 var jsonParser = bodyParser.json();
 var app =  express();
 app.use(jsonParser);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // endpoint section
 const apiRoutes = require('./routes');
@@ -12,3 +12,5 @@ app.use('/', apiRoutes);
 
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+
+// tes
