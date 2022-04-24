2d298ca64c0fe86506594b6e206a3b87b3e6e32c
Abad <abdila.alid@gmail.com>
2022-04-24 20:43:43 +0700

    a

--- MetaKantin.js
+++ MetaKantin.js
@@ -5,7 +5,7 @@ var app =  express();
 var client = require('./db/connection');
 var cookieParser = require('cookie-parser');
 app.use(jsonParser);
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 // app.use(express.static('public'));
