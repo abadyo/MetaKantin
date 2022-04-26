246c8d1202e6b5ec98d69d0d850590a8cd082637
Abad <abdila.alid@gmail.com>
2022-04-27 02:13:50 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -255,28 +255,33 @@ router.post('/api/pay', verifyToken, (req, res, next) => {
                 if(result1.rowCount > 0) {
                     // res.setHeader('Content-Type', 'application/json');
                     // res.end(JSON.stringify(result1.rows[0]["cash"], null, 3)); 
-                    res.setHeader('Content-Type', 'application/json');
-                    res.end(JSON.stringify({
-                        message: req.body.harga,
-                        a: result1.rows[0]["cash"],
-                        b: result1.rows[0]["cash"] > req.body.harga
-                    }, null, 3));
-                    // if(result1.rows[0]["cash"] < req.body.harga) {
-                    //     client.query('UPDATE mk_pengguna SET cash = cash - $1 WHERE username = $2', [req.body.harga, req.username], (error2, result2) => {
-                    //         if(result2.rowCount != 0) {
-                    //             // client.query(' UPDATE mk_kantin SET cash = cash + $1', [req.body.harga]);
-                    //             res.setHeader('Content-Type', 'application/json');
-                    //             res.end(JSON.stringify({ 
-                    //                 message: "Transaksi berhasil" 
-                    //                 }, null, 3)); 
-                    //         } else {
-                    //             res.setHeader('Content-Type', 'application/json');
-                    //             res.end(JSON.stringify({ 
-                    //                 message: "Transaksi gagal" 
-                    //                 }, null, 3)); 
-                    //         }
-                    //     });
-                    // }
+                    // res.setHeader('Content-Type', 'application/json');
+                    // res.end(JSON.stringify({
+                    //     message: req.body.harga,
+                    //     a: result1.rows[0]["cash"],
+                    //     b: result1.rows[0]["cash"] > req.body.harga
+                    // }, null, 3));
+                    if(result1.rows[0]["cash"] > req.body.harga) {
+                        client.query('UPDATE mk_pengguna SET cash = cash - $1 WHERE username = $2', [req.body.harga, req.username], (error2, result2) => {
+                            if(result2.rowCount != 0) {
+                                // client.query(' UPDATE mk_kantin SET cash = cash + $1', [req.body.harga]);
+                                res.setHeader('Content-Type', 'application/json');
+                                res.end(JSON.stringify({ 
+                                    message: "Transaksi berhasil" 
+                                    }, null, 3)); 
+                            } else {
+                                res.setHeader('Content-Type', 'application/json');
+                                res.end(JSON.stringify({ 
+                                    message: "Transaksi gagal" 
+                                    }, null, 3)); 
+                            }
+                        });
+                    } else {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.end(JSON.stringify({ 
+                            message: "Uang kamu tidak cukup" 
+                            }, null, 3)); 
+                    }
                 } else {
                     res.setHeader('Content-Type', 'application/json');
                     res.end(JSON.stringify({ 
